86cce50654a6661d7da8109512ab88ab7b99406e
Aziz Bellaaj <mohamedazizbellaaj@insat.ucar.tn>
2022-08-23 16:18:13 +0100

    CP

---
+++ A Good Contest/A_A_Good_Contest.cpp
@@ -0,0 +1,198 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
+typedef vector<int> v32;
+typedef vector<ll> v64;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef set<int> s32;
+typedef set<ll> s64;
+typedef set<double> sd;
+typedef vector<vector<int>> vv32;
+typedef vector<vector<ll>> vv64;
+typedef vector<vector<p64>> vvp64;
+typedef vector<p32> vp32;
+typedef vector<p64> vp64;
+
+ll MOD = 1000000007;
+#define INF 2e18
+#define EPS 1e-9
+#define ln "\n"
+#define ins insert
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define ook order_of_key  // Number of elements STRICTLY smaller than X
+#define fbo find_by_order //  *ITERATOR* pointing to the kth element (0 order)
+#define all(_x) begin(_x), end(_x)
+#define sz(_x) ((ll)(_x).size())
+#define forn(_i, _e) for (ll _i = 0; _i < _e; ++_i)
+#define forsn(_i, _s, _e) for (ll _i = _s; _i < _e; ++_i)
+#define rforn(_i, _s) for (ll _i = _s; _i >= 0; --_i)
+#define rforsn(_i, _s, _e) for (ll _i = _s; _i >= _e; --_i)
+#define tt     \
+    ll _t;     \
+    cin >> _t; \
+    while (_t--)
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T>
+istream &operator>>(istream &is, vector<T> &intermediate_array)
+{
+    for (auto &a : intermediate_array)
+        cin >> a;
+    return is;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<vector<T>> &intermediate_2dmatrix)
+{
+    for (auto &a : intermediate_2dmatrix)
+        cin >> a;
+    return is;
+}
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> &intermediate_array)
+{
+    for (auto &a : intermediate_array)
+        cout << a << " ";
+    return os;
+}
+template <typename T>
+ostream &operator<<(ostream &os, vector<vector<T>> &intermediate_2dmatrix)
+{
+    for (auto &a : intermediate_2dmatrix)
+        cout << a << ln;
+    return os;
+}
+template <typename T>
+ostream &operator<<(ostream &os, set<T> &intermediate_set)
+{
+    for (auto &a : intermediate_set)
+        cout << a << " ";
+    return os;
+}
+
+template <typename T>
+void print(T a) { cout << a; }
+void print() { cout << " "; }
+template <typename T>
+void println(T a) { cout << a << ln; }
+void println() { cout << ln; }
+template <typename T, typename... Args>
+void print(T a, Args... b)
+{
+    cout << a << " ";
+    print(b...);
+}
+template <typename T, typename... Args>
+void println(T a, Args... b)
+{
+    cout << a << " ";
+    println(b...);
+}
+template <typename T>
+void _dbg(T a)
+{
+    if (typeid(a) == typeid(char))
+        cout << '\'';
+    if (typeid(a) == typeid(string))
+        cout << '\"';
+    cout << a;
+    if (typeid(a) == typeid(char))
+        cout << '\'';
+    if (typeid(a) == typeid(string))
+        cout << '\"';
+    cout << "]" << ln;
+}
+template <typename T, typename... Args>
+void _dbg(T a, Args... b)
+{
+    if (typeid(a) == typeid(char))
+        cout << '\'';
+    if (typeid(a) == typeid(string))
+        cout << '\"';
+    cout << a;
+    if (typeid(a) == typeid(char))
+        cout << '\'';
+    if (typeid(a) == typeid(string))
+        cout << '\"';
+    cout << ",	";
+    _dbg(b...);
+}
+template <typename... Args>
+void dbg(Args... b)
+{
+    cout << '[';
+    _dbg(b...);
+}
+
+void swap(ll &x, ll &y)
+{
+    ll temp = y;
+    y = x;
+    x = temp;
+}
+
+int gcd(int a, int b)
+{
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+ll powa(ll a, ll b, ll m = MOD)
+{
+    if (b == 0)
+        return 1ll;
+    else if (b == 1)
+        return a;
+    else
+    {
+        ll x = powa(a, b / 2, m);
+        x *= x;
+        // x %= m;
+        if (b % 2)
+        {
+            x *= a;
+            // x %= m;
+        }
+        return x;
+    }
+}
+int main()
+{
+    fast_cin();
+    int n;
+    cin >> n;
+    string s;
+    int b, a;
+    forn(i, n)
+    {
+        cin >> s >> b >> a;
+        if (b < a && b >= 2400)
+        {
+            print("YES");
+            return 0;
+        }
+    }
+    print("NO");
+
+    return 0;
+}
---
+++ LeetCode/622.design-circular-queue.cpp
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode id=622 lang=cpp
+ *
+ * [622] Design Circular Queue
+ */
+
+// @lc code=start
+class MyCircularQueue
+{
+public:
+    vector<int> q;
+    int head = 0, tail = -1, size;
+    MyCircularQueue(int k)
+    {
+        size = k;
+        q = vector<int>(size);
+    }
+
+    bool enQueue(int value)
+    {
+        if (isFull())
+        {
+            return false;
+        }
+        tail = (tail + 1) % size;
+        q[tail] = value;
+        return true;
+    }
+
+    bool deQueue()
+    {
+        if (isEmpty())
+        {
+            return false;
+        }
+        if (head == tail)
+        {
+            head = 0, tail = -1;
+        }
+        else
+        {
+            head = (head + 1) % size;
+        }
+        return true;
+    }
+
+    int Front()
+    {
+        return isEmpty() ? -1 : q[head];
+    }
+
+    int Rear()
+    {
+        return isEmpty() ? -1 : q[tail];
+    }
+
+    bool isEmpty()
+    {
+        return tail == -1;
+    }
+
+    bool isFull()
+    {
+        return !isEmpty() && (tail + 1) % size == head;
+    }
+};
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue* obj = new MyCircularQueue(k);
+ * bool param_1 = obj->enQueue(value);
+ * bool param_2 = obj->deQueue();
+ * int param_3 = obj->Front();
+ * int param_4 = obj->Rear();
+ * bool param_5 = obj->isEmpty();
+ * bool param_6 = obj->isFull();
+ */
+// @lc code=end
