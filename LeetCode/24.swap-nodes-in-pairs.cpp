5234d1165f00980b8543b8600d7f850f184c513c
Mohamed Aziz Bellaaj <azizbellaaj9@yahoo.fr>
2022-09-24 13:31:25 +0100

    CP

---
+++ LeetCode/24.swap-nodes-in-pairs.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=24 lang=cpp
+ *
+ * [24] Swap Nodes in Pairs
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *swapPairs(ListNode *head)
+    {
+        if (!head || !head->next)
+        {
+            return head;
+        }
+        ListNode *tmp = head->next;
+        head->next = tmp->next;
+        tmp->next = head;
+        head->next = swapPairs(head->next);
+        return tmp;
+    }
+};
+// @lc code=end
