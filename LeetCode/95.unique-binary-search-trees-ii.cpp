2854e8653f75fa699ad738769959ba0864b7116a
Mohamed Aziz Bellaaj <azizbellaaj9@yahoo.fr>
2022-10-17 14:00:38 +0100

    CP

--- LeetCode/95.unique-binary-search-trees-ii.cpp
+++ LeetCode/95.unique-binary-search-trees-ii.cpp
@@ -18,10 +18,41 @@
  */
 class Solution
 {
+private:
+    vector<TreeNode *> helper(int start, int end)
+    {
+        vector<TreeNode *> out;
+        if (start > end)
+        {
+            out.push_back(nullptr);
+            return out;
+        }
+        for (int i = start; i <= end; i++)
+        {
+            vector<TreeNode *> lefts = helper(start, i - 1);
+            vector<TreeNode *> rights = helper(i + 1, end);
+            for (int j = 0; j < lefts.size(); j++)
+            {
+                for (int k = 0; k < rights.size(); k++)
+                {
+                    TreeNode *root = new TreeNode(i);
+                    root->left = lefts[j];
+                    root->right = rights[k];
+                    out.push_back(root);
+                }
+            }
+        }
+        return out;
+    }
+
 public:
     vector<TreeNode *> generateTrees(int n)
     {
-        
+        if (n == 0)
+        {
+            return vector<TreeNode *>(0);
+        }
+        return helper(1, n);
     }
 };
 // @lc code=end
