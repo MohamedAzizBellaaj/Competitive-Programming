8ca90547759c6f779b92928238cf8ad4c902eb10
Mohamed Aziz Bellaaj <mohamedaziz.bellaaj@insat.ucar.tn>
2023-09-18 18:53:10 +0100

    CP

--- LeetCode/1337.the-k-weakest-rows-in-a-matrix.cpp
+++ LeetCode/1337.the-k-weakest-rows-in-a-matrix.cpp
@@ -1,33 +1,45 @@
 /*
  * @lc app=leetcode id=1337 lang=cpp
  *
- * [1337] The K Weakest Rows in a Matrix
+ * [1337] The K Weakest Rows in rowA Matrix
  */
 
 // @lc code=start
 class Solution
 {
 private:
-    static bool criterion(vector<int> &a, vector<int> &b)
+    int helper(vector<int> &row)
     {
-        int numbSoldiersA = accumulate(a.begin(), a.end(), 0) - a.back();
-        int numbSoldiersB = accumulate(b.begin(), b.end(), 0) - b.back();
-        return numbSoldiersA < numbSoldiersB or (numbSoldiersA == numbSoldiersB and a.back() < b.back());
+        int left = 0, right = row.size(), middle;
+        while (left < right)
+        {
+            middle = left + (right - left) / 2;
+            if (row[middle] == 1)
+            {
+                left = middle + 1;
+            }
+            else
+            {
+                right = middle;
+            }
+        }
+        return right;
     }
 
 public:
     vector<int> kWeakestRows(vector<vector<int>> &mat, int k)
     {
         int n = mat.size();
-        for (int i = 0; i < mat.size(); i++)
+        vector<pair<int, int>> v(n);
+        for (int i = 0; i < n; i++)
         {
-            mat[i].push_back(i);
+            v[i] = {helper(mat[i]), i};
         }
-        sort(mat.begin(), mat.end(), criterion);
+        sort(v.begin(), v.end());
         vector<int> out(k);
         for (int i = 0; i < k; i++)
         {
-            out[i] = mat[i].back();
+            out[i] = v[i].second;
         }
         return out;
     }
