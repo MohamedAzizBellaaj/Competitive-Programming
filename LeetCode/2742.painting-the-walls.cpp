3b35c72c3801d37b26b71558277018112e29c909
Mohamed Aziz Bellaaj <mohamedaziz.bellaaj@insat.ucar.tn>
2023-10-14 14:04:29 +0100

    CP

---
+++ LeetCode/2742.painting-the-walls.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=2742 lang=cpp
+ *
+ * [2742] Painting the Walls
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int paintWalls(vector<int> &cost, vector<int> &time)
+    {
+        int n = cost.size();
+        vector<int> dp = vector(n + 1, 0);
+        vector<int> prevDp = vector(n + 1, (int)1e9);
+        prevDp[0] = 0;
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            dp = vector(n + 1, 0);
+            for (int remain = 1; remain <= n; remain++)
+            {
+                int paint = cost[i] + prevDp[max(0, remain - 1 - time[i])];
+                int dontPaint = prevDp[remain];
+                dp[remain] = min(paint, dontPaint);
+            }
+
+            prevDp = dp;
+        }
+
+        return dp[n];
+    }
+};
+
+// @lc code=end
