6cfbdf68b008ee413f504d6f98e4c1e780efb6f6
Mohamed Aziz Bellaaj <mohamedaziz.bellaaj@insat.ucar.tn>
2023-10-16 13:54:48 +0100

    CP

--- LeetCode/119.pascals-triangle-ii.cpp
+++ LeetCode/119.pascals-triangle-ii.cpp
@@ -7,50 +7,18 @@
 // @lc code=start
 class Solution
 {
-    // private:
-    //     vector<int> helper(int rowIndex, int curRow, vector<int> v)
-    //     {
-    //         if (curRow == rowIndex)
-    //         {
-    //             return v;
-    //         }
-    //         vector<int> out(v.size() + 1);
-    //         out.front() = 1;
-    //         out.back() = 1;
-    //         for (int i = 1; i < out.size() - 1; ++i)
-    //         {
-    //             out[i] = v[i - 1] + v[i];
-    //         }
-    //         return helper(rowIndex, curRow + 1, out);
-    //     }
-
 public:
-    // vector<int> getRow(int rowIndex)
-    // {
-    //     if (rowIndex == 0)
-    //     {
-    //         return {1};
-    //     }
-    //     if (rowIndex == 1)
-    //     {
-    //         return {1, 1};
-    //     }
-    //     vector<int> out = {1, 1};
-    //     return helper(rowIndex, 1, out);
-    // }
     vector<int> getRow(int rowIndex)
     {
-        vector<int> out{1}, curr{1};
-        for (int i = 1; i <= rowIndex; ++i)
+        vector<int> res(1, 1);
+        unsigned long long prev = 1;
+        for (int k = 1; k <= rowIndex; k++)
         {
-            out.push_back(1);
-            for (int j = 1; j < i; ++j)
-            {
-                out[j] = curr[j] + curr[j - 1];
-            }
-            curr = out;
+            unsigned long long next = prev * (rowIndex - k + 1) / k;
+            res.push_back(next);
+            prev = next;
         }
-        return out;
+        return res;
     }
 };
 // @lc code=end
