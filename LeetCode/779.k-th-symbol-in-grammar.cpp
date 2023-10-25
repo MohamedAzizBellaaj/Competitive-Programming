e6a502c5b3a3d337a2196799f09ac9a48bd054b3
Mohamed Aziz Bellaaj <mohamedaziz.bellaaj@insat.ucar.tn>
2023-10-25 14:38:22 +0100

    CP

--- LeetCode/779.k-th-symbol-in-grammar.cpp
+++ LeetCode/779.k-th-symbol-in-grammar.cpp
@@ -10,21 +10,21 @@ class Solution
 public:
     int kthGrammar(int n, int k)
     {
-        if (n == 1 && k == 1)
+        if (n == 1 and k == 1)
         {
             return 0;
         }
+        if (k <= pow(2, n - 1) / 2)
+        {
+            return kthGrammar(n - 1, k);
+        }
+        else if (n % 2 == 1)
+        {
+            return kthGrammar(n - 1, pow(2, n - 1) - k + 1);
+        }
         else
         {
-            int length = pow(2, n - 1);
-            if (k <= length / 2)
-            {
-                return kthGrammar(n - 1, k);
-            }
-            else
-            {
-                return !kthGrammar(n - 1, k - length / 2);
-            }
+            return !kthGrammar(n - 1, pow(2, n - 1) - k + 1);
         }
     }
 };
