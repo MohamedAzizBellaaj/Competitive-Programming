f51c82b410406ee2d7bab60e8be9fc69af7aec76
Mohamed Aziz Bellaaj <mohamedaziz.bellaaj@insat.ucar.tn>
2023-10-31 12:37:14 +0100

    CP

---
+++ LeetCode/2433.find-the-original-array-of-prefix-xor.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=2433 lang=cpp
+ *
+ * [2433] Find The Original Array of Prefix Xor
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> findArray(vector<int> &pref)
+    {
+        int n = pref.size();
+
+        for (int i = n - 1; i > 0; i--)
+        {
+            pref[i] ^= pref[i - 1];
+        }
+
+        return pref;
+    }
+};
+// @lc code=end
