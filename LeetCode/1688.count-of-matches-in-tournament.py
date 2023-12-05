b39fb1a16a443773b137b27d59c57a07f98e7c29
Mohamed Aziz Bellaaj <mohamedaziz.bellaaj@insat.ucar.tn>
2023-12-05 20:10:01 +0100

    CP

--- LeetCode/1688.count-of-matches-in-tournament.py
+++ LeetCode/1688.count-of-matches-in-tournament.py
@@ -8,17 +8,7 @@
 # @lc code=start
 class Solution:
     def numberOfMatches(self, n: int) -> int:
-        result = 0
-
-        while n > 1:
-            if n % 2 == 0:
-                result += n // 2
-                n = n // 2
-            else:
-                result += (n - 1) // 2
-                n = ((n - 1) // 2) + 1
-
-        return result
+        return n - 1
 
 
 # @lc code=end
