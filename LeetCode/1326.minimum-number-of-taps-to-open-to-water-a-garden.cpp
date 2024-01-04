e3436ec7b6e0e7402278c853c8b100aefcbaa11c
Mohamed Aziz Bellaaj <mohamedaziz.bellaaj@insat.ucar.tn>
2024-01-04 19:06:52 +0100

    CP

--- LeetCode/1326.minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ LeetCode/1326.minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -12,14 +12,16 @@ public:
     {
         int maxReach[n + 1];
         memset(maxReach, 0, sizeof(maxReach));
+
         for (int i = 0; i < n + 1; i++)
         {
             int start = max(0, i - ranges[i]);
             int end = min(n, ranges[i] + i);
             maxReach[start] = max(maxReach[start], end);
         }
-        
+
         int taps = 0, currEnd = 0, nextEnd = 0;
+
         for (int i = 0; i < n + 1; i++)
         {
             if (i > nextEnd)
@@ -33,6 +35,7 @@ public:
             }
             nextEnd = max(nextEnd, maxReach[i]);
         }
+
         return taps;
     }
 };
