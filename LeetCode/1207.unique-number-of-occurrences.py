227d8b5f441ccee8998f6ed8002d4c8af0049b9a
Mohamed Aziz Bellaaj <mohamedaziz.bellaaj@insat.ucar.tn>
2024-01-17 21:24:12 +0100

    CP

---
+++ LeetCode/1207.unique-number-of-occurrences.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=1207 lang=python3
+#
+# [1207] Unique Number of Occurrences
+#
+
+
+# @lc code=start
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        arr.sort()
+        v = []
+
+        i = 0
+        while i < len(arr):
+            cnt = 1
+
+            # Count occurrences of the current element
+            while i + 1 < len(arr) and arr[i] == arr[i + 1]:
+                cnt += 1
+                i += 1
+
+            v.append(cnt)
+            i += 1
+
+        v.sort()
+
+        for i in range(1, len(v)):
+            if v[i] == v[i - 1]:
+                return False
+
+        return True
+
+
+# @lc code=end
